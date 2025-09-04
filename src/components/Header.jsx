aff02fff4c0b4fbf9e78ef25b58ecdde425df9cd
Anil Sapkota <muzicmaniac2015@gmail.com>
2025-09-03 22:43:19 -0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="../images/globe.png" className="header-img" alt="globe" />
+            <span className="header-text">my travel journal.</span>
+        </header>
+    )
+}
